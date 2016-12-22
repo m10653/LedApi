00103d3f0cb26b3aca34a74e9ba9083b626606fb
m10653 <mcombsb1@gmail.com>
2016-12-22 17:34:21 -0500

    Added Arduino code

---
+++ Arduino/LedDriver/LedDriver.ino
@@ -0,0 +1,63 @@
+#include "FastLED.h"
+
+// How many leds in your strip?
+#define NUM_LEDS 50
+
+// For led chips like Neopixels, which have a data line, ground, and power, you just
+// need to define DATA_PIN.  For led chipsets that are SPI based (four wires - data, clock,
+// ground, and power), like the LPD8806 define both DATA_PIN and CLOCK_PIN
+#define DATA_PIN 6
+#define CLOCK_PIN 13
+
+// Define the array of leds
+CRGB leds[NUM_LEDS];
+int bytenum = 0;
+byte bytes[5];
+void setup() {
+  Serial.begin(9600);
+  Serial.write(255);
+  // Uncomment/edit one of the following lines for your leds arrangement.
+  // FastLED.addLeds<TM1803, DATA_PIN, RGB>(leds, NUM_LEDS);
+  // FastLED.addLeds<TM1804, DATA_PIN, RGB>(leds, NUM_LEDS);
+  // FastLED.addLeds<TM1809, DATA_PIN, RGB>(leds, NUM_LEDS);
+  FastLED.addLeds<WS2811, DATA_PIN, RGB>(leds, NUM_LEDS);
+  FastLED.show();
+  // FastLED.addLeds<WS2812, DATA_PIN, RGB>(leds, NUM_LEDS);
+  // FastLED.addLeds<WS2812B, DATA_PIN, RGB>(leds, NUM_LEDS);
+  //FastLED.addLeds<NEOPIXEL, DATA_PIN>(leds, NUM_LEDS);
+  // FastLED.addLeds<UCS1903, DATA_PIN, RGB>(leds, NUM_LEDS);
+  // FastLED.addLeds<UCS1903B, DATA_PIN, RGB>(leds, NUM_LEDS);
+  // FastLED.addLeds<GW6205, DATA_PIN, RGB>(leds, NUM_LEDS);
+  // FastLED.addLeds<GW6205_400, DATA_PIN, RGB>(leds, NUM_LEDS);
+
+  // FastLED.addLeds<WS2801, RGB>(leds, NUM_LEDS);
+  // FastLED.addLeds<SM16716, RGB>(leds, NUM_LEDS);
+  // FastLED.addLeds<LPD8806, RGB>(leds, NUM_LEDS);
+
+  // FastLED.addLeds<WS2801, DATA_PIN, CLOCK_PIN, RGB>(leds, NUM_LEDS);
+  // FastLED.addLeds<SM16716, DATA_PIN, CLOCK_PIN, RGB>(leds, NUM_LEDS);
+  // FastLED.addLeds<LPD8806, DATA_PIN, CLOCK_PIN, RGB>(leds, NUM_LEDS);
+}
+
+void loop() {
+  // Turn the LED on, then pause
+  if (Serial.available() > 0) {
+    byte temp = Serial.read();
+    //    Serial.println("temp:" + temp);
+    if (bytenum < 4) {
+      bytes[bytenum++] = temp;
+    } else {
+      bytes[bytenum] = temp;
+      bytenum = 0;
+      int i = bytes[3];
+      byte d = bytes[4];
+      leds[i].red = bytes[0];
+      leds[i].green  = bytes[1];
+      leds[i].blue = bytes[2];
+      //      if (((d >> 1) & 1) != 1) {
+
+      //      if(((d>> 2) & 1) == 1){
+      FastLED.show();
+    }
+  }
+}
