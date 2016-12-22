d5fa2f6a0297c07c166b6206e20b7c4790f75281
m10653 <mcombsb1@gmail.com>
2016-12-22 17:44:47 -0500

    Update Gitignore

--- src/Main.java
+++ src/Main.java
@@ -18,29 +18,6 @@ public class Main {
 		}
 //		}
 		System.exit(0);
-//		controler.show();
-//		serialPort = new SerialPort("COM5");
-//		try {
-//			serialPort.openPort();
-//
-//			serialPort.setParams(SerialPort.BAUDRATE_9600, SerialPort.DATABITS_8, SerialPort.STOPBITS_1, SerialPort.PARITY_NONE);
-//
-//			serialPort.setFlowControlMode(SerialPort.FLOWCONTROL_RTSCTS_IN | SerialPort.FLOWCONTROL_RTSCTS_OUT);
-//
-//			serialPort.addEventListener(new PortReader(), SerialPort.MASK_RXCHAR);
-//			System.out.println(serialPort.readString());
-//			System.out.println(serialPort.isOpened());
-//			
-////			Thread.sleep(3000);
-////			serialPort.writeString("0 0 255 2");
-////			serialPort.writeString("0 0 255 -1");
-//			Scanner scan = new Scanner(System.in);
-//			while(true){
-//				serialPort.writeString(scan.nextLine());
-//			}
-//		} catch (SerialPortException ex) {
-//			System.out.println("There are an error on writing string to port : " + ex);
-//		}
 
 	}
 
